87a59ade1b6a75d38c2f915e2d45addc99b5b7d5
Rouf -Myz <rouf08412@gmail.com>
2025-10-15 15:01:42 +0700

    Fix dev mode and improve comments in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    // sangat beresiko, gunakan hanya pada mode development atau staging jangan gunakan di production dalam jangka waktu yang lama
+   // allowedHosts: ['.'], // mengizinkan semua host yang memiliki domain untuk terhubung ke server pengembangan
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
